20a7838a89fd293653da577ff7622ce252dfe747
Hamid Alizadeh <silvanatnk.com@gmail.com>
2017-01-18 22:45:39 +0330

    add single task api function

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,8 +3,19 @@ var router = express.Router();
 var mongojs = require('mongojs');
 var db = mongojs('mongodb://silvana:Silvana@ds117869.mlab.com:17869/taskemoon',['tasks']);
 
+// Get All Tasks
 router.get('/tasks', function(req, res, next){
     db.tasks.find(function(err, tasks){
+        if(err){
+            res.send(err);
+        }
+        res.json(tasks);
+    });
+});
+
+// Get Single Tasks
+router.get('/task/:id', function(req, res, next){
+    db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, tasks){
         if(err){
             res.send(err);
         }
