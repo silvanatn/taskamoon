8909cf0b0ba25f29fb1d1358bee7fa7f192f971f
Hamid Alizadeh <silvanatnk.com@gmail.com>
2017-01-18 22:40:49 +0330

    connect to mlab mongo database, tasks collection

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://silvana:Silvana@ds117869.mlab.com:17869/taskemoon',['tasks']);
 
 router.get('/tasks', function(req, res, next){
-    res.send('Task API');
+    db.tasks.find(function(err, tasks){
+        if(err){
+            res.send(err);
+        }
+        res.json(tasks);
+    });
 });
 
 module.exports = router;
