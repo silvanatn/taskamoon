5dcfce38ee4f95ddb35cf6d99608aa4a503e8199
Hamid Alizadeh <silvanatnk.com@gmail.com>
2017-01-20 13:23:56 +0330

    add save task route

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var mongojs = require('mongojs');
-var db = mongojs('mongodb://silvana:Silvana@ds117869.mlab.com:17869/taskemoon',['tasks']);
+var db = mongojs('mongodb://silvana:Silvana@ds117869.mlab.com:17869/taskemoon', ['tasks']);
 
 // Get All Tasks
-router.get('/tasks', function(req, res, next){
-    db.tasks.find(function(err, tasks){
-        if(err){
+router.get('/tasks', function (req, res, next) {
+    db.tasks.find(function (err, tasks) {
+        if (err) {
             res.send(err);
         }
         res.json(tasks);
@@ -14,13 +14,31 @@ router.get('/tasks', function(req, res, next){
 });
 
 // Get Single Tasks
-router.get('/task/:id', function(req, res, next){
-    db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, tasks){
-        if(err){
+router.get('/task/:id', function (req, res, next) {
+    db.tasks.findOne({ _id: mongojs.ObjectId(req.params.id) }, function (err, tasks) {
+        if (err) {
             res.send(err);
         }
         res.json(tasks);
     });
+});
+
+// Save Tasks
+router.post('/task', function (req, res, next) {
+    var task = req.body;
+    if (!res.title || (task.isDone + '')) {
+        res.status(400);
+        res.json({
+            "error": "Bad Data"
+        });
+    } else {
+        db.tasks.save(task, function(err, task) {
+            if(err){
+                res.send(err);
+            }
+            res.json(task);
+        });
+    }    
 });
 
 module.exports = router;
