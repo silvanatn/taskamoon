e8126164cde2f366174cee1a37e2fac1ca1db221
Hamid Alizadeh <silvanatnk.com@gmail.com>
2017-01-20 13:32:14 +0330

    add delete and update route

--- routes/tasks.js
+++ routes/tasks.js
@@ -32,13 +32,53 @@ router.post('/task', function (req, res, next) {
             "error": "Bad Data"
         });
     } else {
-        db.tasks.save(task, function(err, task) {
-            if(err){
+        db.tasks.save(task, function (err, task) {
+            if (err) {
                 res.send(err);
             }
             res.json(task);
         });
-    }    
+    }
+});
+
+// Delete Task
+router.delete('/task/:id', function (req, res, next) {
+    db.tasks.remove({ _id: mongojs.ObjectId(req.params.id) }, function (err, tasks) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(tasks);
+    });
+});
+
+// Update Task
+router.delete('/task/:id', function (req, res, next) {
+    var task = req.body;
+    var updTask = {};
+
+    if (task.isDone) {
+        updTask.isDone = task.isDone;
+    }
+
+    if (task.title) {
+        updTask.title = task.title;
+    }
+
+    if (!updTask) {
+        res.status(400);
+        res.json({
+            "error": "Bad Data"
+        });
+    } else {
+        db.tasks.update({ _id: mongojs.ObjectId(req.params.id) }, updTask, {}, function (err, tasks) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(tasks);
+        });
+    }
+
+
 });
 
 module.exports = router;
